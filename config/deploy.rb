ecb65740d4a852f4c1ad3897ae61173a4546cb6e
Tomoya Chiba <tomo.asleep@gmail.com>
2014-09-27 17:35:03 +0900

    Ask branch to deploy when run `cap ${environment} deploy`

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,9 @@ set :deploy_to, '/home/isu-user/isucon-repo'
 # Default value for :scm is :git
 set :scm, :git
 
+# Ask branch to deploy
+set :branch, ask('branch to deploy', 'master')
+
 # Default value for :format is :pretty
 # set :format, :pretty
 
